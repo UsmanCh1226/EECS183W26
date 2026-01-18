8df6e63b2ae7d23279e1ae5efff2074d6d092061
UsmanCh1226 <usmanch@umich.edu>
2026-01-18 14:01:43 -0500

    Imeplemented style guide for project 1

--- Projects/Project1/focaccia.cpp
+++ Projects/Project1/focaccia.cpp
@@ -1,8 +1,8 @@
 /**
  * focaccia.cpp
  *
- * Name: <#Your Name#>
- * Uniqname: <#Your Uniqname#>
+ * Name: <Usman Chaudhry>
+ * Uniqname: <usmanch>
  *
  * EECS 183: Project 1
  * Winter 2026
@@ -16,48 +16,49 @@
 #include <string>
 using namespace std;
 
+
 string pluralize(string singular, string plural, int number);
 
 int main() {
-    int people;
+    int people = 0;
+    
     cout << "How many people do you need to serve? ";
     cin >> people;
     cout << endl << endl;
 
-    // --- Constants  ---
-    const double SERVINGS_PER_LOAF = 4.0;
     
+    const double SERVINGS_PER_LOAF = 4.0;
     const double CUPS_FLOUR_PER_LOAF = 5.0;
     const double CUPS_FLOUR_PER_BAG = 20.0;
     const double PRICE_FLOUR = 2.69;
-    
     const double TSP_YEAST_PER_LOAF = 1.75;
     const double TSP_YEAST_PER_PACKAGE = 2.25;
     const double PRICE_YEAST = 0.40;
-    
     const double TSP_SALT_PER_LOAF = 1.875;
     const double TSP_SALT_PER_CANISTER = 6.0;
     const double PRICE_SALT = 0.49;
-    
     const double TBSP_OIL_PER_LOAF = 2.0;
     const double ML_OIL_PER_BOTTLE = 500.0;
     const double ML_PER_TBSP = 14.8;
     const double PRICE_OIL = 6.39;
     
-    // --- Calculations ---
-    int loaves = ceil(people / SERVINGS_PER_LOAF);
     
-    int flourBags = ceil((loaves * CUPS_FLOUR_PER_LOAF) / CUPS_FLOUR_PER_BAG);
-    int yeastPackages = ceil((loaves * TSP_YEAST_PER_LOAF) / TSP_YEAST_PER_PACKAGE);
-    int saltCanisters = ceil((loaves * TSP_SALT_PER_LOAF) / TSP_SALT_PER_CANISTER);
+    int loaves = static_cast<int>(ceil(people / SERVINGS_PER_LOAF));
+    
+    int flourBags = static_cast<int>(ceil((loaves * CUPS_FLOUR_PER_LOAF) / 
+                                     CUPS_FLOUR_PER_BAG));
+    int yeastPackages = static_cast<int>(ceil((loaves * TSP_YEAST_PER_LOAF) / 
+                                         TSP_YEAST_PER_PACKAGE));
+    int saltCanisters = static_cast<int>(ceil((loaves * TSP_SALT_PER_LOAF) / 
+                                         TSP_SALT_PER_CANISTER));
     
     double tbspPerBottle = ML_OIL_PER_BOTTLE / ML_PER_TBSP;
-    int oilBottles = ceil((loaves * TBSP_OIL_PER_LOAF) / tbspPerBottle);
+    int oilBottles = static_cast<int>(ceil((loaves * TBSP_OIL_PER_LOAF) / 
+                                      tbspPerBottle));
     
     double totalCost = (flourBags * PRICE_FLOUR) + (yeastPackages * PRICE_YEAST) +
                        (saltCanisters * PRICE_SALT) + (oilBottles * PRICE_OIL);
     
-    // --- Final Output ---
     cout << "You need to make: " << loaves << " " 
          << pluralize("loaf", "loaves", loaves) << " of focaccia" << endl << endl;
     
@@ -79,7 +80,6 @@ int main() {
     return 0;
 }
 
-// DO NOT CHANGE THIS FUNCTION
 string pluralize(string singular, string plural, int number) {
     if (number == 1) {
         return singular;
